8ba965e5266f47edd4ee5c474e7475b19ba83274
Victor Frye <victorfrye@outlook.com>
2024-01-04 19:23:57 -0500

    feat: add editorconfig

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using VictorFrye.DotCom.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);

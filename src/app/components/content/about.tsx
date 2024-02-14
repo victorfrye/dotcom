b525eea5080e046392812e3606b03f4068b83876
Victor Frye <victorfrye@outlook.com>
2024-02-13 23:52:12 -0500

    fix: use light text for about content (#28)

--- src/app/components/content/about.tsx
+++ src/app/components/content/about.tsx
@@ -13,21 +13,21 @@ export default function About() {
     }
 
     return (
-        <section id="about" className="d-flex flex-column flex-wrap text-primary pt-3 pb-2">
+        <section id="about" className="d-flex flex-column flex-wrap">
             <h2 className="text-primary pt-3 pb-2">_About</h2>
-                
-                <p>
-                    Hello from Grand Rapids! <b>I&#x27;m Victor, a software engineer powered by coffee</b>.
-                    With {`${getYearsOfExperience()}`} years of experience in the industry, I&#x27;ve worked with a variety of technologies and platforms.
-                    My current focuses are on web development with <strong>React.js</strong>, the <strong>.NET</strong> ecosystem, <strong>Azure</strong> cloud services, and <strong>DevOps</strong> automation and processes.
-                </p>
-                <p>
-                    I&#x27;m passionate about <strong>digital transformation</strong>, the <strong>developer experience</strong>, and constructing solutions that help people <strong>achieve more</strong>.
-                </p>
-                <p>
-                    I&#x27;m currently working as a <strong>Senior Software Engineer at <Link href="https://leadingedje.com" target="_blank" rel="noreferrer"><strong>Leading EDJE</strong></Link></strong>, a software consultancy based in Columbus, Ohio.
-                    In my downtime, I enjoy <b>gaming</b>, <b>reading</b>, and quality time with <b>my wife and our two dogs</b>.
-                </p>
+
+            <p>
+                Hello from Grand Rapids! <b>I&#x27;m Victor, a software engineer powered by coffee</b>.
+                With {`${getYearsOfExperience()}`} years of experience in the industry, I&#x27;ve worked with a variety of technologies and platforms.
+                My current focuses are on web development with <strong>React.js</strong>, the <strong>.NET</strong> ecosystem, <strong>Azure</strong> cloud services, and <strong>DevOps</strong> automation and processes.
+            </p>
+            <p>
+                I&#x27;m passionate about <strong>digital transformation</strong>, the <strong>developer experience</strong>, and constructing solutions that help people <strong>achieve more</strong>.
+            </p>
+            <p>
+                I&#x27;m currently working as a <strong>Senior Software Engineer at <Link href="https://leadingedje.com" target="_blank" rel="noreferrer"><strong>Leading EDJE</strong></Link></strong>, a software consultancy based in Columbus, Ohio.
+                In my downtime, I enjoy <b>gaming</b>, <b>reading</b>, and quality time with <b>my wife and our two dogs</b>.
+            </p>
         </section>
     )
 }

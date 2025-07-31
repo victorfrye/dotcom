f366319592123807f38edb9e4eacf3454a08ccc4
Victor Frye <victorfrye@outlook.com>
2025-07-31 09:51:06 -0400

    feat(apphost): upgrade to aspire 9.4 (#256)

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -2,6 +2,17 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddNpmApp("client", "../WebClient", "dev")
     .WithHttpEndpoint(env: "PORT")
-    .WithExternalHttpEndpoints();
+    .WithUrlForEndpoint("http", static _ => new()
+    {
+        Url = "/resume",
+        DisplayText = "Resume"
+    })
+    .WithUrlForEndpoint("http", static _ => new()
+    {
+        Url = "/blog",
+        DisplayText = "Blog"
+    })
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/");
 
 await builder.Build().RunAsync();

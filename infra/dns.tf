87ec654f319adfde7e8bce36bd83ba580e982d49
Victor Frye <victorfrye@outlook.com>
2025-10-13 12:05:33 -0400

    feat: migrate iac to terraform

---
+++ infra/dns.tf
@@ -0,0 +1,78 @@
+resource "azurerm_dns_zone" "domain" {
+  name                = var.domain_name
+  resource_group_name = data.azurerm_resource_group.dotcom.name
+  tags                = data.azurerm_resource_group.dotcom.tags
+
+  soa_record {
+    email = "azuredns-hostmaster.microsoft.com"
+    ttl   = 3600
+  }
+}
+
+resource "azurerm_dns_ns_record" "name_servers" {
+  name                = "@"
+  resource_group_name = data.azurerm_resource_group.dotcom.name
+
+  zone_name = azurerm_dns_zone.domain.name
+  records   = var.name_servers
+  ttl       = 172800
+}
+
+resource "azurerm_dns_a_record" "swa_alias" {
+  name                = "@"
+  resource_group_name = data.azurerm_resource_group.dotcom.name
+
+  zone_name          = azurerm_dns_zone.domain.name
+  target_resource_id = azurerm_static_web_app.stapp.id
+  ttl                = 3600
+}
+
+resource "azurerm_dns_cname_record" "www" {
+  name                = "www"
+  resource_group_name = data.azurerm_resource_group.dotcom.name
+
+  zone_name = azurerm_dns_zone.domain.name
+  record    = azurerm_static_web_app.stapp.default_host_name
+  ttl       = 3600
+}
+
+resource "azurerm_dns_cname_record" "bing" {
+  name                = var.bing_validation_token
+  resource_group_name = data.azurerm_resource_group.dotcom.name
+
+  zone_name = azurerm_dns_zone.domain.name
+  record    = "verify.bing.com"
+  ttl       = 3600
+}
+
+resource "azurerm_dns_txt_record" "apex" {
+  name                = "@"
+  resource_group_name = data.azurerm_resource_group.dotcom.name
+
+  zone_name = azurerm_dns_zone.domain.name
+  ttl       = 3600
+
+  dynamic "record" {
+    for_each = compact([
+      var.entra_validation_token,
+      var.google_validation_token,
+      azurerm_static_web_app_custom_domain.apex.validation_token
+    ])
+
+    content {
+      value = record.value
+    }
+  }
+}
+
+resource "azurerm_dns_txt_record" "bluesky" {
+  name                = "_atproto"
+  resource_group_name = data.azurerm_resource_group.dotcom.name
+
+  zone_name = azurerm_dns_zone.domain.name
+  ttl       = 3600
+
+  record {
+    value = var.bluesky_validation_token
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,76 @@
+# MARK: Solution
+
+variable "project_name" {
+  description = "The name of the project."
+  type        = string
+  default     = "VictorFrye.COM"
+}
+
+variable "application_name" {
+  description = "The name of the application."
+  type        = string
+  default     = "dotcom"
+}
+
+variable "rg_name" {
+  description = "The name of the resource group."
+  type        = string
+  default     = "rg-dotcom"
+}
+
+variable "owner_name" {
+  description = "The name of the owner."
+  type        = string
+  default     = "victorfrye"
+}
+
+# MARK: Azure Static Web App
+
+variable "swa_sku" {
+  description = "The SKU for the Static Web App."
+  type        = string
+  default     = "Free"
+}
+
+# MARK: Azure DNS
+
+variable "domain_name" {
+  description = "The domain name for the DNS zone."
+  type        = string
+  default     = "victorfrye.com"
+}
+
+variable "name_servers" {
+  description = "The name servers for the DNS zone."
+  type        = list(string)
+  default = [
+    "ns1-32.azure-dns.com.",
+    "ns2-32.azure-dns.net.",
+    "ns3-32.azure-dns.org.",
+    "ns4-32.azure-dns.info.",
+  ]
+}
+
+variable "bing_validation_token" {
+  description = "The Bing validation token."
+  type        = string
+  default     = "11c96e44392f490ee66a7bca4621f35f"
+}
+
+variable "entra_validation_token" {
+  description = "The Entra ID validation token."
+  type        = string
+  default     = "MS=ms98140114"
+}
+
+variable "google_validation_token" {
+  description = "The Google validation token."
+  type        = string
+  default     = "google-site-verification=GszGq7kP4hWb1k8UXPzSZO_nvRMqzrY9W6cmAxEJ7h4"
+}
+
+variable "bluesky_validation_token" {
+  description = "The Bluesky validation token."
+  type        = string
+  default     = "did=did:plc:nfa5vksskl6vjbw57a2irgob"
+}

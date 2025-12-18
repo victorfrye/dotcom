c22f1840853392356a9b3fc1c9967eaae94d49ef
Victor Frye <victorfrye@outlook.com>
2025-12-18 12:33:17 -0500

    build(deps): bump packages + add svgr

---
+++ src/WebClient/app/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as BlueskyIcon } from './bluesky.svg';
+export { default as DiscordIcon } from './discord.svg';
+export { default as GitHubIcon } from './github.svg';
+export { default as LinkedInIcon } from './linkedin.svg';
+export { default as ThreadsIcon } from './threads.svg';
+export { default as XboxIcon } from './xbox.svg';
